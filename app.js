e3b930437d92cbffeef11e2636247e317dcb3983
Awood549 <Austin.wood1776@live.com>
2019-08-18 11:55:22 -0700

    commiting to submit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const io = require('socket.io-client');
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://aw-socket-server.us-east-2.elasticbeanstalk.com/');
 
 const faker = require('faker');
 
